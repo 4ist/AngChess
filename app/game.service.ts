82affc6668d66fa743b3bd6c87946bd476ac6bba
4ist <frichardson062@gmail.com>
2019-07-15 08:56:41 -0500

    Uploaded components/services

---
+++ app/game.service.ts
@@ -0,0 +1,199 @@
+import { Injectable, EventEmitter } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameService {
+
+  lastSelected = ""
+  moveHistory = [];
+
+  ids = [];
+  initialState = [];
+  boardState = {}
+  pieceMap = {
+
+    //white pawns
+    "A2": "\u2659",
+    "B2": "\u2659",
+    "C2": "\u2659",
+    "D2": "\u2659",
+    "E2": "\u2659",
+    "F2": "\u2659",
+    "G2": "\u2659",
+    "H2": "\u2659",
+
+    //black pawns
+    "A7": "\u265F",
+    "B7": "\u265F",
+    "C7": "\u265F",
+    "D7": "\u265F",
+    "E7": "\u265F",
+    "F7": "\u265F",
+    "G7": "\u265F",
+    "H7": "\u265F",
+
+    //Rooks
+    "A1": "\u2656", "H1": "\u2656",
+    "A8": "\u265C", "H8": "\u265C",
+
+    //Knights
+    "B1": "\u2658", "G1": "\u2658",
+    "B8": "\u265E", "G8": "\u265E",
+
+    //Bishops
+    "C1": "\u2657", "F1": "\u2657",
+    "C8": "\u265D", "F8": "\u265D",
+
+    //Queens
+    "D1": "\u2655",
+    "D8": "\u265B",
+
+    //Kings
+    "E1": "\u2654",
+    "E8": "\u265A",
+
+  }
+
+  wPieces = ["\u2659", "\u2656", "\u2658", "\u2657", "\u2655", "\u2654"];
+  bPieces = ["\u265F", "\u265C", "\u265E", "\u265D", "\u265B", "\u265A"];
+  
+  wGrave = [];
+  bGrave = [];
+  constructor() {
+    
+    var alphs = ["A", "B", "C", "D", "E", "F", "G", "H"];
+    for (var j = 8; j >= 1; j--) {
+      for (var i = 0; i < 8; i++) {
+        this.ids.push(alphs[i] + j);
+        this.initialState.push();
+      }
+    }
+
+
+    for (var i = 0; i < 64; i++) {
+      this.boardState[this.ids[i]] = this.initialState[i];
+    }
+    this.reset();
+    console.log(this.bPieces);
+    console.log(this.wPieces);
+  }
+  ngOnInit() {
+    
+  }
+  
+  //raiseEvent() {
+  //  console.log("raise event");
+  //  this.receivedFilter.emit();
+  //}
+
+  reset() { 
+    for (var i = 0; i < this.ids.length; i++) {
+      var tempTile = document.getElementById(this.ids[i]);
+
+      //other pieces
+      if (this.pieceMap[this.ids[i]]) {
+        this.boardState[this.ids[i]]  = this.pieceMap[this.ids[i]]
+      }
+      //empty tile
+      else {
+        this.boardState[this.ids[i]]  = "";
+      }
+    }
+    this.wGrave = [];
+    this.bGrave = [];
+    console.log("Board was reset");
+
+  }
+
+  makeMove(src, dst) {
+    //brute force movement method
+    console.log("moving:" + src + ">" + dst)
+    this.moveHistory.push([src, this.boardState[src],
+      dst, this.boardState[dst]]);
+
+    //add dst piece to grave
+    if (this.bPieces.includes(this.boardState[dst])){
+      this.bGrave.push(this.boardState[dst]);
+    }
+    else if (this.wPieces.includes(this.boardState[dst])){
+      this.wGrave.push(this.boardState[dst]);
+    }
+
+    //move piece
+    this.boardState[dst] = this.boardState[src];
+    this.boardState[src] = "";
+    console.log(this.bGrave);
+    console.log(this.wGrave);
+ 
+  }
+
+  undo() {
+    //undo last move from moveHistory array
+    if (this.moveHistory.length < 1) {
+      //TODO: throw error message 
+      console.log("null undo")
+    }
+
+    if (this.moveHistory.length > 0) { 
+      console.log("undoing last move");
+
+      var lastMove = this.moveHistory.pop();
+
+      for (var i = 0; i < this.bGrave.length; i++) {
+        if (this.bGrave[i] == lastMove[3]) {
+          delete this.bGrave[i];
+          break
+        }
+      }
+      for (var i = 0; i < this.wGrave.length; i++) {
+        if (this.wGrave[i] == lastMove[3]) {
+          delete this.wGrave[i];
+          break
+        }
+      }
+      
+      
+
+      console.log(lastMove);
+      //lastMove = [srcTile, srcPiece, dstTile, dstPiece]
+      this.boardState[lastMove[0]] = lastMove[1];
+      this.boardState[lastMove[2]] = lastMove[3];
+    }
+  }
+
+
+  adminFillGrave() {
+
+    this.wGrave = ["\u2659", "\u2659", "\u2659", "\u2659", "\u2659", "\u2659", "\u2659", "\u2659",
+                  "\u2656", "\u2658", "\u2657",
+                  "\u2656", "\u2658", "\u2657",
+                  "\u2655", "\u2654"];
+
+    this.bGrave = ["\u265F", "\u265F", "\u265F", "\u265F", "\u265F", "\u265F", "\u265F", "\u265F",
+                  "\u265C", "\u265E", "\u265D",
+                  "\u265C", "\u265E", "\u265D",
+                  "\u265B", "\u265A"];
+
+  }
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
---
+++ app/sidebar/sidebar.component.ts
@@ -0,0 +1,170 @@
+import { Component, OnInit } from '@angular/core';
+import { BoardComponent } from '../board/board.component';
+import { GameService } from "../game.service";
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  themeOpened = false;
+  dark = "#BFD3C1";
+  light = "#FFE5D4";
+  currentMenuNav = "";
+  currentMenuView = "";
+  currentThemeView = "";
+  loopingThemes = false;
+
+  constructor(private serv: GameService) {}
+
+  ngOnInit(): void { }
+
+  toggleNav(nav, view, navLabel) {
+    console.log("nav:" + nav + " last nav:" + this.currentMenuNav);
+    console.log("view:" + view + " last view:" + this.currentMenuView);
+   
+    if (nav == this.currentMenuNav) {
+      document.getElementById(this.currentMenuView).hidden = true;
+      this.currentMenuNav = "";
+      this.currentMenuView = "";
+      this.closeNav(nav);
+    }
+    else {
+      console.log("else");
+      if (this.currentMenuNav != "" && this.currentMenuView != "") {
+        document.getElementById(this.currentMenuNav).style.backgroundColor = 'var(--light)';
+        document.getElementById(this.currentMenuView).hidden = true;
+      }
+      document.getElementById('navLabel').innerText = navLabel;
+      this.currentMenuNav = nav;
+      this.currentMenuView = view;
+      this.openNav(nav);
+    }
+  }
+
+  openNav(nav) {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("sidebar").style.marginLeft = "250px";
+    document.getElementById(nav).style.backgroundColor = 'var(--dark)';
+    document.getElementById(this.currentMenuView).hidden = false;
+  }
+
+  closeNav(nav) {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("sidebar").style.marginLeft = "0px";
+    document.getElementById(nav).style.backgroundColor = 'var(--light)'; 
+
+    //close themes also
+    this.themeOpened = false;
+    document.getElementById("themeItems").style.height = "0px";
+    document.getElementById("openThemes").style.color = "#818181";
+  }
+
+
+  toggleThemes(container, label) {
+    console.log("Current view:" + this.currentThemeView);
+    console.log("Loading view:" + container);
+
+    if (container == this.currentThemeView) {
+      console.log("closing");
+      this.currentThemeView = "";
+      this.closeThemeMenu(container, label)
+    }
+    else {
+      if (this.currentThemeView != "") {
+        console.log("close current menu");
+        this.closeThemeMenu(this.currentThemeView, label);
+        this.currentThemeView = container;
+        
+      }
+      console.log("opening new menu");
+      this.currentThemeView = container;
+      this.openThemeMenu(container, label); 
+    }
+  }
+
+  openThemeMenu(container, label) {
+    document.getElementById(container).style.height = "100px";
+    document.getElementById(label).style.color = "#f1f1f1";
+    this.themeOpened = true;
+  }
+
+  closeThemeMenu(container, label) {
+    this.themeOpened = false;
+    document.getElementById(container).style.height = "0px";
+    document.getElementById(label).style.color = "#818181";
+  }
+
+  setTheme(light, dark) {
+    this.light = light;
+    this.dark = dark;
+    console.log("Light:" + light);
+    console.log("Dark:" + dark);
+    document.documentElement.style.setProperty('--light', light);
+    document.documentElement.style.setProperty('--dark', dark);
+  }
+
+  setCustomTheme() {
+    var light = document.getElementById("lightTxt").value;
+    var dark = document.getElementById("darkTxt").value;
+
+    console.log("Light:" + light);
+    console.log("Dark:" + dark);
+
+    this.setTheme(light, dark);
+  }
+
+  setRandomTheme() {
+    var lmin = 100;
+    var lmax = 200;
+
+    var dmin = 50
+    var dmax = 150;
+
+    var lr = Math.floor(Math.random() * (+lmax - +lmin) + +lmin).toString(16); 
+    var lg = Math.floor(Math.random() * (+lmax - +lmin) + +lmin).toString(16); 
+    var lb = Math.floor(Math.random() * (+lmax - +lmin) + +lmin).toString(16); 
+    var light = "#" + lr + lg + lb;
+    
+    var dr = Math.floor(Math.random() * (+dmax - +dmin) + +dmin).toString(16); 
+    var dg = Math.floor(Math.random() * (+dmax - +dmin) + +dmin).toString(16); 
+    var db = Math.floor(Math.random() * (+dmax - +dmin) + +dmin).toString(16); 
+    var dark = "#" + dr + dg + db;
+
+    this.setTheme(light, dark);
+  }
+
+  toggleLoopTheme() {
+    console.log('toggle loop theme');
+    console.log(this.loopingThemes);
+    if (this.loopingThemes == false) {
+      this.loopingThemes = true;
+      document.getElementById("loopTheme").style.color = "#f1f1f1";
+      this.setRandomTheme();
+      this.loopTheme();
+    }
+    else {
+      this.loopingThemes = false;
+      document.getElementById("loopTheme").style.color = "#818181";
+    }
+  }
+  
+  loopTheme() {
+    console.log('loopem');
+    setTimeout(() => {
+      if (this.loopingThemes) {
+        this.setRandomTheme();
+        this.loopTheme();
+      }
+    }, 5000);
+  } 
+
+
+
+
+
+
+
+
+}
